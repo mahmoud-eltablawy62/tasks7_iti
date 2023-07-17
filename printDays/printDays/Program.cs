cd46cf1177632d0961b9d437ed4814916385f913
mahmoud-eltablawy62 <134718324+mahmoud-eltablawy62@users.noreply.github.com>
2023-07-17 13:19:45 +0300

    tasksDat6

---
+++ printDays/printDays/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace printDays
+{
+
+    enum days
+    {
+        saturday,
+        sunday,
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday,
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             
+            bool IsFound = false;
+            do
+            {
+                Console.WriteLine("enter the number of day ===> ");
+                int num_day = int.Parse(Console.ReadLine());
+                switch (num_day)
+                {
+                    case 1:
+                        Console.WriteLine("day===>" + days.saturday);
+                        IsFound = false;
+                        break;
+                    case 2:
+                        Console.WriteLine("day===>" + days.sunday);
+                        IsFound = false;
+                        break;
+                    case 3:
+                        Console.WriteLine("day===>" + days.monday);
+                        IsFound = false;
+                        break;
+                    case 4:
+                        Console.WriteLine("day===>" + days.tuesday);
+                        IsFound = false;
+                        break;
+                    case 5:
+                        Console.WriteLine("day===>" + days.wednesday);
+                        IsFound = false;
+                        break;
+                    case 6:
+                        Console.WriteLine("day===>" + days.thursday);
+                        IsFound = false;
+                        break;
+                    case 7:
+                        Console.WriteLine("day===>" + days.friday);
+                        IsFound = false;
+                        break;
+                    default:
+                        Console.WriteLine("enter answer number");
+                        IsFound = true;
+                        break;
+                }
+            } while (IsFound == true);
+
+        }
+    }
+}
